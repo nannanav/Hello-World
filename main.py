c7f29129f9311c854d6c637c2811967d21a1d711
knakatasf <knakata@dons.usfca.edu>
2025-08-21 15:59:00 -0700

    feature: create hello world logic

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+def print_hi(name):
+    print(f"Hello, {name}")
 
 
 # Press the green button in the gutter to run the script.
